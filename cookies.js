6e3eadbfb6a059796f65478fa9d30c3e2fe1420d
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-07-05 15:33:03 +0200

    Poprawione napisy na komunikacie o ciasteczkach. Teraz jest po polsku.

--- cookies.js
+++ cookies.js
@@ -1,11 +1,16 @@
 window.addEventListener("load", function(){
-    window.cookieconsent.initialise({
-      "palette": {
-        "popup": {
-          "background": "#000"
-        },
-        "button": {
-          "background": "#f1d600"
-        }
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
       }
-    })});
+    },
+    "content": {
+      "message": "Nasza strona internetowa używa plików cookies (tzw. ciasteczka) w celach statystycznych, reklamowych oraz funkcjonalnych. Dzięki nim możemy indywidualnie dostosować stronę do twoich potrzeb. ",
+      "dismiss": "Zrozumiałem!",
+      "link": "Kliknij po więcej"
+    }
+  })});
