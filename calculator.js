c52dc602f2cd335eb7fb0f608badb287e709917b
agata okolska <agata.okolska@gmail.com>
2018-07-18 17:09:18 +0200

    add functions to calculator depending on type of user

--- calculator.js
+++ calculator.js
@@ -1,19 +1,38 @@
-function calculatePrice (){
+function calculatePrice() {
+    const render = () => {
+        totalPrice.innerText = usersNumber * 5 * (isCompany ? 1 : 0) + ' $'
+        numberOfUsers.innerText = 'Ilość użytkowników: ' + numberOfCompanyUsers.value
+    }
+    
     const individualClient = document.querySelector('.calculator__button_individual')
     const companyClient = document.querySelector('.calculator__button_company')
-    const numberOfCompanyUsers = document.querySelector('.tickmarks')
+    const numberOfCompanyUsers = document.querySelector('#rangeinput')
     const totalPrice = document.querySelector('.total_price')
+    const numberOfUsers = document.querySelector('#number-of-users')
 
-
-
-    individualClient.addEventListener('click', function(){
-       return totalPrice.innerHTML = '0 zł'
-    })
-    companyClient.addEventListener('click', function(){
-        return totalPrice.innerHTML = '5 zł'
-     })
+    let usersNumber = null
+    let isCompany = false
     
+
+    individualClient.addEventListener('click', function () {
+        isCompany = false
+        render()
+    })
+    companyClient.addEventListener('click', function () {
+        isCompany = true
+
+        render()
+    })
+
+    numberOfCompanyUsers.addEventListener("input", function () {
+        usersNumber = numberOfCompanyUsers.value
+        render()
+    })
+
 
 }
 
+
 calculatePrice()
+
+
